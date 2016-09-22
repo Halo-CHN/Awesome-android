489291e1178b4ba56af04cfb1b01354d0d4ec34b
Halo-CHN <halo-chn@outlook.com>
2016-09-22 13:39:30 +0800

    all new

---
+++ tabar/src/main/java/com/chn/halo/tabar/HaloFragmentManager.java
@@ -0,0 +1,154 @@
+package com.chn.halo.tabar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Halo-CHN
+ * @version 1.0
+ * @description 自定义 FragmentManager
+ * @mail halo-chn@outlook.com
+ * @date 2015年7月8日
+ */
+public class HaloFragmentManager {
+
+    public HaloFragmentManager(HaloViewPager pViewPager,
+                               FragmentManager pFragmentManager) {
+        fragmentManager = pFragmentManager;
+        viewPager = pViewPager;
+        fragmetsMap = new HashMap<>();
+        fragments = new ArrayList<>();
+        bTextViews = new ArrayList<>();
+        viewPager.setSlipping(false);
+        viewPager.setOffscreenPageLimit(5);
+        haloPagerAdapter = new HaloPagerAdapter();
+        viewPager.setAdapter(haloPagerAdapter);
+        /* 点击底部导航切换时触发事件 */
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < getBottomTextViews().size(); i++) {
+                    getBottomTextViews().get(i).setTextViewSelected(
+                            i == position);
+                }
+                SelectableBottomTextViewAttributesEx.onSelectableTextViewID = getBottomTextViews()
+                        .get(position).getId();
+            }
+
+            @Override
+            public void onPageScrolled(int position, float arg1, int arg2) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int position) {
+            }
+        });
+    }
+
+    private Map<Integer, Fragment> fragmetsMap;
+
+    private static HaloViewPager viewPager;
+
+    private static List<Fragment> fragments;
+
+    private static List<SelectableBottomTextView> bTextViews;
+
+    private static FragmentManager fragmentManager;
+
+    private HaloPagerAdapter haloPagerAdapter;
+
+    public void addFragment(SelectableBottomTextView key, Fragment valueFragment) {
+        if (!fragmetsMap.containsKey(key.getId())) {
+            if (fragmetsMap.size() == 0) {
+                SelectableBottomTextViewAttributesEx.onSelectableTextViewID = key
+                        .getId();
+            }
+            fragmetsMap.put(key.getId(), valueFragment);
+            fragments.add(valueFragment);
+            bTextViews.add(key);
+        }
+        haloPagerAdapter.notifyDataSetChanged();
+    }
+
+    public void clickToChangeFragment(int id) {
+        if (!(SelectableBottomTextViewAttributesEx.onSelectableTextViewID == id)) {
+            for (SelectableBottomTextView selectableBottomTextView : bTextViews) {
+                if (selectableBottomTextView.getId() == id) {
+                    viewPager
+                            .setCurrentItem(bTextViews
+                                    .indexOf(selectableBottomTextView), false);
+                }
+            }
+            SelectableBottomTextViewAttributesEx.onSelectableTextViewID = id;
+        }
+    }
+
+    public Map<Integer, Fragment> getFragmentMap() {
+        return this.fragmetsMap;
+    }
+
+    public List<Fragment> getFragments() {
+        return fragments;
+    }
+
+    public List<SelectableBottomTextView> getBottomTextViews() {
+        return bTextViews;
+    }
+
+    public static class HaloPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public void destroyItem(View container, int position, Object object) {
+            ((ViewPager) container).removeView(fragments.get(position)
+                    .getView());
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Fragment fragment = fragments.get(position);
+            if (!fragment.isAdded()) {
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.add(fragment, fragment.getClass().getSimpleName());
+                ft.commit();
+                /**
+                 * 在用FragmentTransaction.commit()方法提交FragmentTransaction对象后
+                 * 会在进程的主线程中，用异步的方式来执行。 如果想要立即执行这个等待中的操作，就要调用这个方法（只能在主线程中调用）。
+                 * 要注意的是，所有的回调和相关的行为都会在这个调用中被执行完成，因此要仔细确认这个方法的调用位置。
+                 */
+                fragmentManager.executePendingTransactions();
+            }
+
+            if (fragment.getView().getParent() == null) {
+                container.addView(fragment.getView());
+            }
+            return fragment.getView();
+        }
+    }
+}
---
+++ tabar/src/main/java/com/chn/halo/tabar/SelectableBottomTextView.java
@@ -0,0 +1,90 @@
+package com.chn.halo.tabar;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewParent;
+import android.widget.TextView;
+
+
+/**
+ * @author Halo-CHN
+ * @version 1.0
+ * @description 自定义底部导航可选择 TextView 控件
+ * @mail halo-chn@outlook.com
+ * @date 2015年7月8日
+ */
+public class SelectableBottomTextView extends TextView implements View.OnClickListener {
+    private boolean selectedDefault = false;
+    private int selectedColor;
+    private int unselectedColor;
+    private int iconSelectedColorFilter;
+    private int iconUnSelectedColorFilter;
+    private Drawable selectedIcon;
+    private Drawable unselectedIcon;
+    private OnSelectableTextViewClickedListener listener;
+
+    public SelectableBottomTextView(Context context) {
+        super(context);
+    }
+
+    public SelectableBottomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+		/* 获取XML布局文件中定义的属性值 */
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SelectableView);
+
+        selectedColor = a.getColor(R.styleable.SelectableView_text_selected_color, 0x000000);
+        unselectedColor = a.getColor(R.styleable.SelectableView_text_unselected_color, 0x000000);
+        iconSelectedColorFilter = a.getColor(R.styleable.SelectableView_icon_selected_color_filter, 0x000000);
+        iconUnSelectedColorFilter = a.getColor(R.styleable.SelectableView_icon_unselected_color_filter, 0x000000);
+        selectedIcon = a.getDrawable(R.styleable.SelectableView_selected_icon);
+
+        unselectedIcon = a.getDrawable(R.styleable.SelectableView_unselected_icon);
+        selectedDefault = a.getBoolean(R.styleable.SelectableView_default_selected, selectedDefault);
+
+        setTextViewSelected(selectedDefault);
+        setOnClickListener(this);
+        a.recycle();
+    }
+
+    public void setTextViewSelected(boolean isSelected) {
+        this.selectedDefault = isSelected;
+        setTextViewSelected();
+    }
+
+    public void setOnTextViewClickedListener(OnSelectableTextViewClickedListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        ViewParent parent = getParent();
+        if (parent instanceof BottomTextViewGroup) {
+            BottomTextViewGroup group = (BottomTextViewGroup) parent;
+            group.onTextViewSelectedChanged(group, getId());
+        }
+        if (listener != null) {
+            listener.onTextViewClicked(view);
+        }
+    }
+
+    private void setTextViewSelected() {
+        if (selectedDefault) {
+            setTextColor(selectedColor);
+            if (null != selectedIcon) {
+                selectedIcon.setColorFilter(iconSelectedColorFilter, PorterDuff.Mode.SRC_ATOP);
+                setCompoundDrawablesWithIntrinsicBounds(null, selectedIcon, null, null);
+            }
+        } else {
+            setTextColor(unselectedColor);
+            if (null != unselectedIcon) {
+                unselectedIcon.setColorFilter(iconUnSelectedColorFilter, PorterDuff.Mode.SRC_ATOP);
+                setCompoundDrawablesWithIntrinsicBounds(null, unselectedIcon, null, null);
+            }
+        }
+    }
+}
